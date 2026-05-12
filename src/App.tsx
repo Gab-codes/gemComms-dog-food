95ec439eb86e174fc5686540c468b62ac7844982
Temidayo Gabriel <dayotoru@gmail.com>
2026-05-12 11:31:37 -0400

    minor update and clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import Stats from "./components/stats";
 
 function App() {
   return (
-    <>
+    <main className="overflow-x-hidden">
       <Hero />
       <section className="bg-[#F8F8F8]">
         <Stats />
       </section>
       <Benefits />
-    </>
+    </main>
   );
 }
 
